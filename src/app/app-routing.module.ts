ddb307e876a5f6149a048ab43a7ee55bcf5f30a4
humayun777 <gl3127@myamu.ac.in>
2020-10-26 01:40:35 +0530

    Add files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MilkComponent } from './milk/milk.component';
+
+
+const routes: Routes = [
+  {
+  path:'',
+  component:HomeComponent
+  },
+  {
+  path:'gallery',
+  component:GalleryComponent
+  },
+  {
+  path:'milk',
+  component:MilkComponent
+  },
+  {
+  path:'home',
+  component:HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ src/app/milk/milk.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MilkComponent } from './milk.component';
+
+describe('MilkComponent', () => {
+  let component: MilkComponent;
+  let fixture: ComponentFixture<MilkComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MilkComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MilkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
